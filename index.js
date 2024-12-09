db1ec9988f0bebba1f1279eae58ab2854a4852b9
Sophiaazaliah <Sophiaazaliah10@gmail.com>
2024-12-09 09:19:17 +0000

    changes

---
+++ index.js
@@ -0,0 +1,19 @@
+let name ="Sophia Azaliah";
+console.log(name);
+let age =89;
+console.log(age);
+let sum =10+15;
+console.log(sum);
+let difference = 20 -5;
+console.log(difference);
+let divided =50/5;
+console.log(divide);
+let product =7*6;
+let remainer =15% 4;
+console.log(remainder);
+let student = false;
+console.log(student);
+let variable = 150;
+console.log(typeof(variable));
+variable ="hello,Where is my food?;"
+console.log(typeof(variable));
